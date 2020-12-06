76e89d091ce693492bc64698a3ff293b5b3a2953
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2020-12-06 16:53:59 +0100

    Collect Words React Native

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { Component} from 'react';
+ 
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+
+const AppButton = ({ onPress, title }) => (
+    <TouchableOpacity onPress={onPress} style={styles.AppButtonContainer}>
+      <Text style={styles.AppButtonText}>{title}</Text>
+    </TouchableOpacity>
+);
+
+class App extends Component {
+
+    constructor(props){
+      super(props)
+      
+      this.state = {
+        wordInput: '',
+        showWords: false,
+        words: []
+      }
+    }
+
+    render(){
+        return(
+        <View style={{padding: 50,}}>
+            <Text style={{ fontSize: 24 }}>Collect Words</Text>
+            <TextInput style={{marginVertical: 10, padding: 5, borderWidth: 1, borderColor: "whitesmoke"}} 
+                onChangeText={(text) => this.setState({wordInput:text})} name="wordInput" placeholder="Word" />
+            <AppButton title="Add Word" onPress={() => this.state.words.push(this.state.wordInput)} />
+            <AppButton title="Delete Word(s)" onPress={() => this.setState({words: []})}  />
+            <AppButton title="Show Word(s)" onPress={() => this.setState({showWords: !this.state.showWords})} />
+            <Text>{this.state.showWords && this.state.words.toString()}</Text>
+        </View>
+        );    
+    }
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    AppButtonContainer:{
+        backgroundColor: 'lightgrey',
+        padding: 5,
+        marginBottom: 10,
+    },
+
+    AppButtonText:{
+        color: 'black',
+        fontSize: 15,
+        fontWeight: "300"
+    }
 });
+
+export default App;
